fabce2d8da250197c937b9e0a48f1843b2bf3462
Navneet Karnani <navneetkarnani@gmail.com>
2016-05-22 18:43:33 +0530

    Table array

--- table-array/table-array.go
+++ table-array/table-array.go
@@ -3,10 +3,18 @@ package main
 import "fmt"
 
 func main() {
+	var array [11][11]int
 	for count := 1; count <= 10; count++ {
 		for multiple := 1; multiple <= 10; multiple++ {
-			fmt.Printf("%d x %d = %d\n", count, multiple, count*multiple)
+			array[count][multiple] = count * multiple
 		}
-		fmt.Println()
+
+		for count := 1; count < len(array); count++ {
+			for multiple := 1; multiple < len(array[0]); multiple++ {
+				fmt.Printf("%d x %d = %d\n", count, multiple, array[count][multiple])
+			}
+			fmt.Println()
+		}
+
 	}
 }
