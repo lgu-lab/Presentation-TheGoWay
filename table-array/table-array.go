2f9f84dd3ec6ee9eaf2786a80b07d3ae27d5221e
Navneet Karnani <navneetkarnani@gmail.com>
2016-05-22 18:54:24 +0530

    With separate functions

--- table-array/table-array.go
+++ table-array/table-array.go
@@ -2,9 +2,15 @@ package main
 
 import "fmt"
 
-func computeTable(count int, slice []int) {
-	for multiple := 1; multiple <= 10; multiple++ {
-		slice[multiple] = count * multiple
+func computeTable(number int, max int, slice []int) {
+	for multiple := 1; multiple <= max; multiple++ {
+		slice[multiple] = number * multiple
+	}
+}
+
+func printTable(n int, array []int) {
+	for multiple := 1; multiple < len(array); multiple++ {
+		fmt.Printf("%d x %d = %d\n", n, multiple, array[multiple])
 	}
 }
 
@@ -16,14 +22,11 @@ const (
 func main() {
 	var array [countMax + 1][multMax + 1]int
 	for count := 1; count <= countMax; count++ {
-		for multiple := 1; multiple <= multMax; multiple++ {
-			array[count][multiple] = count * multiple
-		}
+		computeTable(1, multMax, array[count][:])
 	}
-	for count := 1; count < len(array); count++ {
-		for multiple := 1; multiple < len(array[0]); multiple++ {
-			fmt.Printf("%d x %d = %d\n", count, multiple, array[count][multiple])
-		}
-		fmt.Println()
+
+	for count := 1; count <= countMax; count++ {
+		printTable(count, array[count][:])
 	}
+
 }
