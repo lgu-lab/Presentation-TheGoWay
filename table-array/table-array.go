21a7be39226b79e3cff48d39fe033afe79c0690d
Navneet Karnani <navneetkarnani@gmail.com>
2016-05-22 18:44:22 +0530

    Table array

--- table-array/table-array.go
+++ table-array/table-array.go
@@ -8,13 +8,11 @@ func main() {
 		for multiple := 1; multiple <= 10; multiple++ {
 			array[count][multiple] = count * multiple
 		}
-
-		for count := 1; count < len(array); count++ {
-			for multiple := 1; multiple < len(array[0]); multiple++ {
-				fmt.Printf("%d x %d = %d\n", count, multiple, array[count][multiple])
-			}
-			fmt.Println()
+	}
+	for count := 1; count < len(array); count++ {
+		for multiple := 1; multiple < len(array[0]); multiple++ {
+			fmt.Printf("%d x %d = %d\n", count, multiple, array[count][multiple])
 		}
-
+		fmt.Println()
 	}
 }
