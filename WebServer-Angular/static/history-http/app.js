aad0feb683bf7111ec8e2ace87f78586bd9b898c
Navneet Karnani <navneetkarnani@gmail.com>
2016-06-04 01:29:49 +0530

    Working History page

--- WebServer-Angular/static/history-http/app.js
+++ WebServer-Angular/static/history-http/app.js
@@ -1,14 +1,26 @@
 var app = angular.module('app', ['ngRoute']);
 
 app.config(function ($routeProvider) {
-  $routeProvider.when('/about', {
+  $routeProvider.when('/', {
     templateUrl: 'about.html',
     controller: 'aboutController'
   });
+
+  $routeProvider.when('/history', {
+    templateUrl: 'history.html',
+    controller: 'historyController'
+  });
+
 });
 
 app.controller('aboutController', function ($scope, $http) {
   $http.get('/api/').success(function (data) {
     $scope.ApiVersion = data.Version
+  });
+});
+
+app.controller('historyController', function ($scope, $http) {
+  $http.get('/api/history').success(function (data) {
+    $scope.History = data.History
   });
 });
