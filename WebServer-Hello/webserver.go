f9003c43c1eb70dbde7fa75ee8a737e11870a66d
Navneet Karnani <navneetkarnani@gmail.com>
2016-06-03 20:28:56 +0530

    Hello without interface

--- WebServer-Hello/webserver.go
+++ WebServer-Hello/webserver.go
@@ -6,12 +6,29 @@ import (
 	"net/http"
 )
 
-func httpHandler(w http.ResponseWriter, r *http.Request) {
+var historyDb = []int{1, 2, 3, 4, 5}
+
+func httpHistory(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "{ History: %v }", historyDb)
+}
+
+func httpHello(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Hello World")
 }
 
 func main() {
-	http.HandleFunc("/", httpHandler)
+
+	http.HandleFunc("/history", httpHistory)
+	http.HandleFunc("/", httpHello)
+	{
+		// http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		// 	if r.URL.Path != "/" {
+		// 		http.NotFound(w, r)
+		// 		return
+		// 	}
+		// 	httpHandler(w, r)
+		// })
+	}
 	fmt.Println("Starting server on port 8080")
 	log.Fatal(http.ListenAndServe("localhost:8080", nil))
 }
