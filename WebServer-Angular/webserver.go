4d00231fbe35b03c228c5d6d73c1389a5c7b84ea
Navneet Karnani <navneetkarnani@gmail.com>
2016-06-03 22:42:10 +0530

    First commit for Angular web server with static page serving

---
+++ WebServer-Angular/webserver.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+// SECTION: Start API Handlers Section
+
+func addAPIRoutes(apiRouter *mux.Router) {
+	apiRouter.Path("/history").Methods("GET").HandlerFunc(httpHistory)
+	apiRouter.HandleFunc("/compute/{n}/{m}", httpCompute)
+
+	//Version is the structure for the Version API
+	type Version struct {
+		Version string
+	}
+	version := Version{Version: "v1"}
+	apiRouter.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		json.NewEncoder(w).Encode(version)
+	})
+
+}
+
+// END SECTION
+
+func httpHello(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello World")
+}
+
+func main() {
+	router := mux.NewRouter().StrictSlash(true)
+	addAPIRoutes(router.PathPrefix("/api").Subrouter())
+	router.PathPrefix("/").Handler(http.FileServer(http.Dir("./static/")))
+
+	fmt.Println("Starting server on port 8080")
+	log.Fatal(http.ListenAndServe("localhost:8080", router))
+}
